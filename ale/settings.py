d1923845d1f54a65e812f0fefd42fa1ad14066cc
merawi <merawiyohannes@gmail.com>
2025-04-02 02:25:36 -0700

    setting modified

--- ale/settings.py
+++ ale/settings.py
@@ -42,7 +42,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["ale-shopping.onrender.com"]
+ALLOWED_HOSTS = ["ale-shopping.onrender.com", "*"]
 
 
 # Application definition
