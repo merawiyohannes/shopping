f82398c87f0d91e16f548d44b44e7cf1fd7ebe94
merawi <merawiyohannes@gmail.com>
2025-04-03 04:38:49 -0700

    database modified

--- ale/settings.py
+++ ale/settings.py
@@ -15,6 +15,7 @@ import cloudinary.uploader
 import cloudinary.api
 from pathlib import Path
 from dotenv import load_dotenv
+import dj_database_url
 
 load_dotenv()
 
@@ -99,12 +100,18 @@ WSGI_APPLICATION = 'ale.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+if os.getenv("DJANGO_PRODUCTION") == "true":  
+    DATABASES = {
+        'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
     }
-}
+else:  
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
